beff25898f1e12d265a9cf8ab6e52ac3282d6305
Danyabah <tedrikyt881@gmail.com>
2023-11-21 00:18:29 +0300

    video and scope

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const introTitle = new SplitType(".intro__title");
 const techTitle = new SplitType(".tech__title");
 const audTitle = new SplitType(".aud__title");
 const formTitle = new SplitType(".form__title");
+const scopTitle = new SplitType(".scope__title");
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -26,6 +27,8 @@ timeline
     },
     "0.4"
   );
+
+// popup
 
 gsap.from(".tech__text", {
   y: "100%",
@@ -58,6 +61,30 @@ gsap.from(".tech .section__item", {
   duration: 0.5,
   ease: "ease",
   scrollTrigger: ".tech .section__row",
+});
+//scope
+gsap.from(".scope__text", {
+  y: "100%",
+  opacity: 0,
+  duration: 0.5,
+  ease: "ease",
+  scrollTrigger: ".scope__text",
+});
+gsap.from(".scope__title .word", {
+  y: "100%",
+  opacity: 0,
+  stagger: 0.05,
+  duration: 0.5,
+  ease: "ease",
+  scrollTrigger: ".scope__title",
+});
+gsap.from(".scope__subtitle", {
+  y: "100%",
+  duration: 0.5,
+  delay: 0.5,
+  ease: "ease",
+  opacity: 0,
+  scrollTrigger: ".scope__subtitle",
 });
 // aud
 gsap.from(".aud__text", {
@@ -136,16 +163,122 @@ gsap.to(".form .inp", {
   ease: "ease",
   scrollTrigger: ".form .form__item",
 });
-const video = document.querySelector(".video__item");
+
 const videoPlay = document.querySelector(".video__play");
 const videoPlayBtn = document.querySelector(".video__play-img");
+const videoPoster = document.querySelector(".video__poster");
+const popup = document.querySelector(".popup");
 
-videoPlayBtn.addEventListener("click", (e) => {
-  if (video.paused) {
-    videoPlay.classList.remove("active");
-    video.play();
-  } else {
-    videoPlay.classList.add("active");
-    video.pause();
+popup.addEventListener("click", (event) => {
+  if (event.target === popup) {
+    // gsap.to(".popup", {
+    //   opacity: 0,
+    //   duration: 0.5,
+    // });
+    // gsap.to(".popup__video-col", {
+    //   y: "-100%",
+    //   duration: 0.5,
+    // });
+    // setTimeout(() => {
+    popup.classList.add("hidden");
+    // }, 500);
+    showPreview();
+    pause();
   }
 });
+
+function showPreview() {
+  videoPoster.classList.remove("hidden");
+  videoPlay.classList.add("active");
+}
+
+function hidePreview() {
+  videoPoster.classList.add("hidden");
+  videoPlay.classList.remove("active");
+}
+
+videoPlayBtn.addEventListener("click", (e) => {
+  popup.classList.remove("hidden");
+  gsap.from(".popup", {
+    opacity: 0,
+    duration: 0.5,
+  });
+  gsap.from(".popup__video-col", {
+    y: "-100%",
+    duration: 0.5,
+  });
+  hidePreview();
+  play();
+});
+
+const scopeItems = document.querySelectorAll(".scope__item");
+
+scopeItems.forEach((item) => {
+  item.onclick = function () {
+    clearScopeClass();
+    item.classList.add("scope__item-active");
+  };
+});
+
+function clearScopeClass() {
+  scopeItems.forEach((item) => {
+    item.classList.remove("scope__item-active");
+  });
+}
+
+// video
+
+const popupPlay = document.querySelector(".popup__play");
+const popupPause = document.querySelector(".popup__pause");
+const popupVideo = document.querySelector(".popup__video-item");
+const progress = document.querySelector(".popup__range");
+
+const time = document.querySelector(".popup__time");
+
+time.innerHTML = sToStr(+popupVideo.duration);
+
+popupPlay.onclick = play;
+
+popupPause.onclick = pause;
+
+popupVideo.ontimeupdate = progressUpdate;
+
+progress.onclick = videoRewind;
+
+function play() {
+  popupVideo.play();
+  popupPause.classList.remove("hidden");
+  popupPlay.classList.add("hidden");
+}
+
+function pause() {
+  popupVideo.pause();
+  popupPause.classList.add("hidden");
+  popupPlay.classList.remove("hidden");
+}
+
+function progressUpdate() {
+  let d = popupVideo.duration;
+  let c = popupVideo.currentTime;
+  progress.value = (100 * c) / d;
+
+  time.innerHTML = sToStr(
+    Math.round(+popupVideo.duration - +popupVideo.currentTime)
+  );
+}
+
+function videoRewind(event) {
+  let w = this.offsetWidth;
+  let o = event.offsetX;
+
+  this.value = (100 * o) / w;
+  pause();
+  popupVideo.currentTime = popupVideo.duration * (o / w);
+  play();
+}
+
+function sToStr(s) {
+  let m = Math.trunc(s / 60) + "";
+  s = (s % 60) + "";
+  return m.padStart(2, 0) + ":" + s.padStart(2, 0);
+}
